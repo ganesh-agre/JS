293416359846702de16ba763655da5ab343586b5
ganesh-agre <ganeshagre20@gmail.com>
2023-12-12 15:05:57 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOpeation.js
@@ -0,0 +1,21 @@
+let score =  true;
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33ab" =>> NAN
+//true => 1
+//false => 0
+// null => 0
+//undefined => NAN
+
+//Boolean
+// 1 => true
+// 0 => false
+// ""=> false
+//"ganesh" => true
+
+//String
