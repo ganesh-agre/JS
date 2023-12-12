b8c3da573dad9941c0577b04027fd4477cdb7168
ganesh-agre <ganeshagre20@gmail.com>
2023-12-12 15:45:37 +0000

    comparison basics

--- 01_basics/03_conversionOpeation.js
+++ 01_basics/03_conversionOpeation.js
@@ -1,9 +1,9 @@
 let score =  true;
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //"33" => 33
 //"33ab" =>> NAN
@@ -19,3 +19,16 @@ console.log(valueInNumber);
 //"ganesh" => true
 
 //String
+
+
+// ****************************** Operations ******************************************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue)
+
+let str1 = "hello";
+let str2 = " ganesh";
+let str3 = str1 + str2;
+//console.log(str3);
+
