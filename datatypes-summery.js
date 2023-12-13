addaf1bc36f42e81fc394c73dac0feb200847448
ganesh-agre <ganeshagre20@gmail.com>
2023-12-13 13:12:03 +0000

    datatype summery

---
+++ datatypes-summery.js
@@ -0,0 +1,18 @@
+// PRIMITIVES
+//7 types:  String, Number, Boolean, Undefined, Null, symbol, BigInt
+
+// Refernce (Non Primitive)
+
+// Array, Objetcs, Functions
+
+const heroes =["shaktiman","nagraj","doga"];
+
+let person ={
+    name:"ganesh",
+    age:22
+};
+
+const myFunction = function(){ 
+    console.log("Hello India");
+}
+
